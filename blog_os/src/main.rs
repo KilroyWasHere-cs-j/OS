5d2a4e6ce8d552bfcf7858be01fc64db748d3cc2
Gabriel Tower <gabriel.tower@baxter-academy.org>
2023-09-08 17:05:47 -0400

    Working
    Things appear to be magically working

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -3,6 +3,12 @@
 
 //  qemu-system-x86_64 -drive format=raw,file=/home/gabrielt/Git/OS/blog_os/target/x86_64-blog_os/debug/bootimage-blog_os.bin
 
+// ? To deploy 
+// ? cargo bootimage
+// ? qemu-system-x86_64 -drive format=raw,file=target/x86_64-blog_os/debug/bootimage-blog_os.bin
+
+// https://os.phil-opp.com/
+
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
@@ -11,10 +17,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+static HELLO: &[u8] = b"Test";
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn print(){
     let vga_buffer = 0xb8000 as *mut u8;
 
     for (i, &byte) in HELLO.iter().enumerate() {
@@ -24,5 +29,10 @@ pub extern "C" fn _start() -> ! {
         }
     }
 
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    print();
     loop {}
 }
